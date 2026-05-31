fd475c59dd946dbbb61ec0cb2446854061a4755e
paopao1812 <paolavalenciavictoria@gmail.com>
2026-05-31 17:52:17 +0200

    feat: Home.css premium styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 
 import { useNavigate } from "react-router-dom";
 import { useUser } from "../context/UserContext";
+import "../styles/Home.css";
 
 export default function Home() {
   const navigate = useNavigate();
