e67a76453358d93a535562d0d8cb4490b3011c5c
paopao1812 <paolavalenciavictoria@gmail.com>
2026-06-18 13:46:30 +0200

    cleanup supabase config

--- src/supabase.jsx
+++ src/supabase.jsx
@@ -1,6 +1,15 @@
+
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl) {
+  throw new Error("VITE_SUPABASE_URL no está configurada");
+}
+
+if (!supabaseKey) {
+  throw new Error("VITE_SUPABASE_ANON_KEY no está configurada");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
